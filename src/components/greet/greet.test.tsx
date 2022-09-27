5980de2a72ed98a765e728d50a380880ea477a1d
Muhtasim Munem Mugdho <muhtasim.mugdho@bjitgroup.com>
2022-09-27 17:16:03 +0600

    initial commit

---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "./greet";
+
+
+describe("Greet", ()=>{
+    test("renders correctly", () => {
+        render(<Greet />);
+        const textElement = screen.getByText("greet");
+        expect(textElement).toBeInTheDocument();
+      });
+
+      test("renders a name", () => {
+        render(<Greet name="mominul"/>);
+        const textElement = screen.getByText(/mominul/i);
+        expect(textElement).toBeInTheDocument();
+      });
+})
+ 
---
+++ src/components/greet/greet.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  name?: string;
+};
+
+const Greet: React.FC<Props> = ({ name }) => {
+  return (
+    <div>
+      greet
+      <div>hello {name?name:"Stranger"}</div>
+    </div>
+  );
+};
+export default Greet;
